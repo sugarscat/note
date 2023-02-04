0f74cc7101aed3b2b9d90cf14e8144bbef93ddaa
Sugarscat <tsugarcat@gmail.com>
2023-02-04 19:31:54 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 import { defineUserConfig } from 'vuepress'
 import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
-
+import { defaultTheme } from 'vuepress'
 export default defineUserConfig({
     base: '/note/',
     publicPath: '/note/',
     lang: 'zh-CN',
-    title: '目录',
+    title: 'Note',
     description: '这是我的笔记。',
     head: [['link', { rel: 'icon', href: 'favicon.ico' }]],
     port: 3001,
@@ -17,4 +17,26 @@ export default defineUserConfig({
             level: [2, 3, 4, 5],
         },
     },
+    theme: defaultTheme({
+        // 导航栏
+        navbar: [
+            {
+                text: '目录',
+                link: '/',
+            },
+            {
+                text: '仓库',
+                children: [
+                    {
+                        text: 'Gitee',
+                        link: 'https://gitee.com/Sugarscat/note',
+                    },
+                    {
+                        text: 'Github',
+                        link: 'https://github.com/Sugarscat/note',
+                    },
+                ],
+            },
+        ],
+    }),
 })
