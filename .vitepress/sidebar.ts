6e026bc6c6b7d18377c40156f25f828acd78ab68
Sugarscat <work@sugarscat.cn>
2024-02-28 12:01:13 +0800

    Revert "添加：语言代码表"
    This reverts commit 528580db286ece2e4236fc60290c7de4263efbfe.

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -25,9 +25,8 @@ import {
     gitIcon,
     spiderIcon,
     regularExpressionIcon,
-    starIcon,
-    earthIcon
-} from './icons'
+    starIcon
+} from './icons.ts'
 
 export default  [
         {
@@ -42,7 +41,6 @@ export default  [
                 {text: starIcon + '软件设计师', link: '/code/SoftwareDesigner.md'},
                 {text: machineLearningIcon + '机器学习', link: '/code/MachineLearning.md'},
                 {text: spiderIcon + '爬虫', link: '/code/WebCrawling.md'},
-                {text: earthIcon + '语言代码表', link: '/code/LanguageCode.md'},
             ]
         },
         {
