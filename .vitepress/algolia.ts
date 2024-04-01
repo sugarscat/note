9fc04a6fa916ca702520de6dbe203dd2b72cc456
Sugarscat <work@sugarscat.cn>
2024-04-01 21:24:44 +0800

    feat(搜索引擎更新，部分包升级): 更换搜索引擎为algolia，升级一些包

---
+++ .vitepress/algolia.ts
@@ -0,0 +1,11 @@
+export default {
+  appId: 'CULZXWV8L0',
+  apiKey: 'c21e0e64e5c1c01d151dd1eb3602f79c',
+  indexName: 'note',
+  placeholder: '请输入关键词',
+  translations: {
+    button: {
+      buttonText: '搜索文档',
+    },
+  },
+}
