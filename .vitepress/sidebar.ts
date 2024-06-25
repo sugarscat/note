c1df936c4c1cd854ecc15af07a6a2ea386828e91
Sugarscat <work@sugarscat.cn>
2024-06-25 11:02:16 +0800

    Update sidebar.ts

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -173,7 +173,7 @@ export default  [
     items: [
       {text: vscodeIcon + 'VS Code 基本使用', link: '/tools/vs-code.md'},
       {text: keyboardIcon + '腹灵F12 68键 RGB', link: '/tools/F12.md'},
-      {text: keyboardIcon + '高漫数位板', link: '/tools/Gaomon.md'},
+      {text: '高漫数位板', link: '/tools/Gaomon.md'},
     ]
   },
 ]
